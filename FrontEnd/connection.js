93f6c029e841043076e1fe7e3a4277186d0aafdd
AmandineUhl <didine709@hotmail.com>
2023-05-31 11:39:27 +0200

    pb fetch

--- FrontEnd/connection.js
+++ FrontEnd/connection.js
@@ -1,5 +1,40 @@
-let email = document.querySelector('#email').value;
-let password = document.querySelector('#password').value;
+const butonConnect = document.querySelector('#submit');
+    butonConnect.addEventListener('click', function(event){
+        event.preventDefault();
+   
+
+const user = {
+    email : document.querySelector('#email').value,
+    password : document.querySelector('#password').value
+};
+
+const dataBase = JSON.stringify(user);
+
+fetch("http://localhost:5678/api/users/login", {
+    method: 'POST',
+    Headers:{'Content_type': 'aplication/json'},
+    body: dataBase,
+})
+
+.then(res => {
+    if (res.status == 404){
+        alert("unknown user")
+    } else {
+        return res.json()
+    };
+    
+})
+.then(responseAuth => {
+
+    sessionStorage.setItem("token", responseAuth.token);
+    sessionStorage.setItem("userId", responseAuth.userId);
+    window.location.href ='./index.html';
+  
+})
+
+.catch(err => console.log(err));
+});
+
 
 
 
