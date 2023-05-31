6f9f90713317c2c098703f103ffb9e049ad15716
AmandineUhl <didine709@hotmail.com>
2023-05-31 16:10:28 +0200

    page connection

--- FrontEnd/connection.js
+++ FrontEnd/connection.js
@@ -1,38 +1,37 @@
 const butonConnect = document.querySelector('#submit');
-    butonConnect.addEventListener('click', function(event){
-        event.preventDefault();
-   
 
-const user = {
-    email : document.querySelector('#email').value,
-    password : document.querySelector('#password').value
-};
+butonConnect.addEventListener('click', async function(event){
 
-const dataBase = JSON.stringify(user);
+    event.preventDefault();
 
-fetch("http://localhost:5678/api/users/login", {
-    method: 'POST',
-    Headers:{'Content_type': 'aplication/json'},
-    body: dataBase,
-})
-
-.then(res => {
-    if (res.status == 404){
-        alert("unknown user")
-    } else {
-        return res.json()
+    const user = {
+        email : document.querySelector('#email').value,
+        password : document.querySelector('#password').value
     };
+
+    const dataBase = JSON.stringify(user);
+
+    const response = await fetch ("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers:{
+            "content-type": "application/json"},
+        body: dataBase,
+    })
+
+    const successConnection = (response) => {
+        sessionStorage.setItem("token", response.token);
+        sessionStorage.setItem("userId", response.userId);
+        window.location.href ='./index.html';
+    }
+
+    const errorConnection = async (response) => {
+        const errorMessage = await response.json()
+        console.log(errorMessage)
+    }
     
-})
-.then(responseAuth => {
-
-    sessionStorage.setItem("token", responseAuth.token);
-    sessionStorage.setItem("userId", responseAuth.userId);
-    window.location.href ='./index.html';
-  
-})
-
-.catch(err => console.log(err));
+    if (response.status === 200) successConnection (response)
+    else if(response.status === 401) errorConnection (response)
+    else if (response.status === 404) errorConnection (response)
 });
 
 
