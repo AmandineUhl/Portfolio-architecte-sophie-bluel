900fe164ea55f8b0eb72f7132ecfffff662a847c
AmandineUhl <didine709@hotmail.com>
2023-06-16 12:35:01 +0200

    pb api

--- FrontEnd/javascript/modale.js
+++ FrontEnd/javascript/modale.js
@@ -4,7 +4,6 @@ function modal1() {
   const accessModale = document.getElementById("boutonModif");
   const modalClosed = document.getElementById("nav_closed");
   const nextModale = document.getElementById("add_pictures");
-  const works = document.getElementById("works");
 
   // Gestion de la navigation
 
@@ -31,85 +30,87 @@ function modal1() {
 
   // Affichage de la galerie
 
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((datas) => {
-      works.innerHTML = "";
-      datas.forEach((data) => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
+  function viewGalery() {
+    fetch("http://localhost:5678/api/works")
+      .then((response) => response.json())
+      .then((datas) => {
+        works.innerHTML = "";
+        datas.forEach((data) => {
+          const figure = document.createElement("figure");
+          const img = document.createElement("img");
+          const figcaption = document.createElement("figcaption");
 
-        img.src = data.imageUrl;
-        figcaption.textContent = "éditer";
+          img.src = data.imageUrl;
+          figcaption.textContent = "éditer";
 
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        works.appendChild(figure);
+          figure.appendChild(img);
+          figure.appendChild(figcaption);
+          works.appendChild(figure);
 
-        const deleteIcon = document.createElement("i");
-        deleteIcon.className = "fa-solid fa-trash-can";
-        figure.appendChild(deleteIcon);
+          const deleteIcon = document.createElement("i");
+          deleteIcon.className = "fa-solid fa-trash-can";
+          figure.appendChild(deleteIcon);
 
-        works.appendChild(figure);
-        
+          //suppression image par image
 
-        //supression image par image
+          deleteIcon.addEventListener("click", () => {
+            const imageId = datas.id;
+            const token = sessionStorage.getItem("token");
 
-        deleteIcon.addEventListener("click", () => {
-          const imageId = datas.id;
-          const token = sessionStorage.getItem("token");
-
-          fetch(`http://localhost:5678/api/works/${imageId}`, {
-            method: "DELETE",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          })
-            .then((response) => {
-              if (response.ok) {
-                figure.remove();
-              }
+            fetch(`http://localhost:5678/api/works/${imageId}`, {
+              method: "DELETE",
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
             })
-            .catch((error) => {
-              console.error(
-                "Erreur lors de la suppression de l'image :",
-                error
-              );
-            });
-        });
+              .then((response) => {
+                if (response.ok) {
+                  figure.remove();
+                }
+              })
+              .catch((error) => {
+                console.error(
+                  "Erreur lors de la suppression de l'image :",
+                  error
+                );
+              });
+          });
 
-        //supression de toute la galerie
+          //suppression de toute la galerie
 
-        const allSupprime = document.getElementById("all_supprime");
+          const allSupprime = document.getElementById("all_supprime");
 
-        allSupprime.addEventListener("click", () => {
-          const imageId = datas.id;
-          const token = sessionStorage.getItem("token");
+          allSupprime.addEventListener("click", () => {
+            const imageId = datas.id;
+            const token = sessionStorage.getItem("token");
 
-          fetch(`http://localhost:5678/api/works/${imageId}`, {
-            method: "DELETE",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          })
-            .then((response) => {
-              if (response.ok) {
-                works.innerHTML = "";
-              }
+            fetch(`http://localhost:5678/api/works/${imageId}`, {
+              method: "DELETE",
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
             })
-            .catch((error) => {
-              console.error(
-                "Erreur lors de la suppression de la galerie :",
-                error
-              );
-            });
+              .then((response) => {
+                if (response.ok) {
+                  works.innerHTML = "";
+                }
+              })
+              .catch((error) => {
+                console.error(
+                  "Erreur lors de la suppression de la galerie :",
+                  error
+                );
+              });
+          });
         });
       });
-    });
+  }
+
 }
 
 modal1();
+
+
 
 function modal2() {
   const modal1 = document.getElementById("modal1");
@@ -139,12 +140,15 @@ function modal2() {
   });
 }
 
-//preview 
+//preview
 
-const imageInput = document.getElementById('input-photo');
-const imagePreviewContainer = document.querySelector('.window_add');
+const imageInput = document.getElementById("input-photo");
+const imagePreviewContainer = document.querySelector(".window_add");
+const icon = document.querySelector(".icon");
+const add = document.querySelector(".add");
+const size = document.querySelector(".size");
 
-imageInput.addEventListener('change', () => {
+imageInput.addEventListener("change", () => {
   if (imageInput.files && imageInput.files[0]) {
     const file = imageInput.files[0];
     const maxSizeInBytes = 4 * 1024 * 1024; // 4 Mo
@@ -156,13 +160,49 @@ imageInput.addEventListener('change', () => {
 
     const reader = new FileReader();
     reader.onload = (e) => {
-      const imgElement = document.createElement('img');
+      const imgElement = document.createElement("img");
       imgElement.src = e.target.result;
-      imagePreviewContainer.classList.remove('hidden');
+      imagePreviewContainer.classList.remove("hidden");
       imagePreviewContainer.appendChild(imgElement);
+      icon.style.display ="none";
+      size.style.display ="none";
+      add.style.display ="none";
     };
 
     reader.readAsDataURL(file);
   }
 });
 
+//select categorie
+
+const apiCategories = async () => {
+  try {
+    await fetch("http://localhost:5678/api/categories")
+      .then((response) => response.json())
+      .then((categoriesResponse) => {
+        categories = categoriesResponse;
+      });
+  } catch (error) {
+    console.log("Erreur à l'affichage des catégories :", error);
+  }
+}
+
+async function showCategory() {
+  const selectCategory = document.querySelector("#categorie");
+  try {
+    await apiCategories();
+    for (let category of categories) {
+      let option = document.createElement("option");
+      option.value = category.id;
+      option.innerText = category.name;
+      selectCategory.appendChild(option);
+    }
+  } catch (error) {
+    console.log(
+      "Erreur lors de la récupération des catégories de la liste d'option :",
+      error
+    );
+  }
+}
+
+showCategory();
