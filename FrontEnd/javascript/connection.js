1b4f2fe588c3c31c2c3524c821c31d734034d518
AmandineUhl <didine709@hotmail.com>
2023-06-22 16:50:09 +0200

    indentation html

--- FrontEnd/javascript/connection.js
+++ FrontEnd/javascript/connection.js
@@ -1,19 +1,19 @@
-const buttonConnect = document.querySelector('#submit');
-const errorMessage = document.querySelector('.error-message'); 
+const buttonConnect = document.querySelector("#submit");
+const errorMessage = document.querySelector(".error-message");
 
-buttonConnect.addEventListener('click', async (event) => {
+buttonConnect.addEventListener("click", async (event) => {
   event.preventDefault();
 
   const user = {
-    email: document.querySelector('#email').value,
-    password: document.querySelector('#password').value
+    email: document.querySelector("#email").value,
+    password: document.querySelector("#password").value,
   };
 
   try {
     const response = await fetch("http://localhost:5678/api/users/login", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
     });
@@ -22,13 +22,13 @@ buttonConnect.addEventListener('click', async (event) => {
       const data = await response.json();
       sessionStorage.setItem("token", data.token);
       sessionStorage.setItem("userId", data.userId);
-      window.location.href = './index.html';
+      window.location.href = "./index.html";
     } else {
       errorMessage.textContent = "Erreur dans l’identifiant ou le mot de passe";
-      buttonConnect.classList.add("shake"); 
+      buttonConnect.classList.add("shake");
 
       setTimeout(() => {
-        buttonConnect.classList.remove("shake"); 
+        buttonConnect.classList.remove("shake");
       }, 500);
     }
   } catch (error) {
